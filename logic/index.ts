33e3c717f21fe34345672fcd60080fbf279ff1fb
Dave Caruso <dave@davecode.me>
2019-10-21 17:31:00 -0400

    Add a base to the logic and ui sections.

--- logic/index.ts
+++ logic/index.ts
@@ -1,1 +1,17 @@
-// ???
+// Stonks and Slonks Logic: Main File
+// This file contains all the functions and data that the UI can access
+
+/** Starts the game. */
+export function startGame() {
+  throw new Error('Can\'t start game since no game exists.');
+}
+/** Gets the main info. */
+export function getGameInfo() {
+  throw new Error('Can\'t get game info since no game exists.');
+}
+/** Simulates one ??? unit of time. Should be called every 10 seconds. */
+export function simulateGame() {
+  throw new Error('Can\'t simulate the game since no game exists.');
+}
+
+// ... add more game actions
--- ui/index.ts
+++ ui/index.ts
@@ -1,1 +1,10 @@
+// Stonks and Slonks UI: Main File
+// It is currently a test file.
+import * as logic from '../logic';
+
 // ???
+logic.startGame();
+
+setInterval(() => {
+  logic.simulateGame();
+}, 10000);
